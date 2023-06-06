d5a93912449da70ad21422d4e0c86e903bd725c2
Khalid Chowdhury <Khalidchowdhury90@gmail.com>
2023-06-06 20:27:36 +0600

    full update

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
 </p>
 
-## Laravel School Management System 
+## About Laravel
 
 Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
 
@@ -21,9 +21,7 @@ Laravel is a web application framework with expressive, elegant syntax. We belie
 
 Laravel is accessible, powerful, and provides tools required for large, robust applications.
 
-## Project Overview
-
-<p align="center"><a><img src="SMS.jpg" width="100%" alt="Laravel Logo"></a></p>
+## Learning Laravel
 
 Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
 
--- routes/web.php
+++ routes/web.php
@@ -1,51 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\frontendController;
-use App\Http\Controllers\Auth\authController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-// School Management system
-
-
-
-// Frontend Route setup
-Route::get('/', [frontendController::class, 'homePage']) -> name('home.page');
-Route::get('/about', [frontendController::class, 'aboutPage']) -> name('about.page');
-Route::get('/courses', [frontendController::class, 'coursesPage']) -> name('courses.Page');
-Route::get('/contact', [frontendController::class, 'contactPage']) -> name('contact.Page');
-
-
-
-// Dashboard Route setup
-
-
-// Register Auth setup 
-Route::get('/register', [authController::class, 'showRegPage']) -> name('register.page');
-Route::post('/register', [authController::class, 'RegPage']) -> name('register.store');
-
-// Register as a teacher
-Route::get('/teacher-register', [authController::class, 'showDoctorRegPage']) -> name('docReg.page');
-// Route::post('/teacher-register', [authController::class, 'doctorRegPage']) -> name('docReg.store');
-
-// Login Auth setup 
-Route::get('login', [authController::class, 'showLoginPage']) -> name('login.page');
-
-
-
-
-// Management system main show page 
-// Route::get('/dashboard', [authController::class, 'showIndexPage']) -> name('index.page');
-
-
-
-
-
-
-
-
-
-
-
-
-
+Route::get('/', function () {
+    return view('welcome');
+});
