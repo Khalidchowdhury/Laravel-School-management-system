b1eb94267e1c31109352c58e4378244a5e497ef7
Khalid Chowdhury <Khalidchowdhury90@gmail.com>
2023-06-15 15:32:15 +0600

    event

--- resources/views/features/event.blade.php
+++ resources/views/features/event.blade.php
@@ -22,7 +22,7 @@
     <div class="row align-items-center">
     <div class="col"></div>
     <div class="col-auto text-end float-end ms-auto">
-    <a href="add-events.html" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+    <a href="add-events.html" class="btn btn-primary">Add Announcement <i class="fas fa-plus"></i></a>
     </div>
     </div>
     </div>
@@ -31,7 +31,30 @@
     <div class="col-lg-12 col-md-12">
     <div class="card">
     <div class="card-body">
-    <div id="calendar"></div>
+        
+
+
+        <!-- Hero -->
+        <div class="p-4 shadow-4 rounded-3" style="background-color: hsl(0, 0%, 94%);">
+            <h2>বার্ষিক বনভোজন </h2>
+            <p>
+                রাজশাহী বিশ্ববিদ্যালয়ের গণযোগাযোগ ও সাংবাদিকতা বিভাগের প্রাক্তন শিক্ষার্থীদের সংগঠন জার্নালিজম অ্যালামনাই ফোরাম অব রাজশাহী ইউনিভার্সিটির (জাফরু) বার্ষিক বনভোজন অনুষ্ঠিত হয়েছে। দিনভর আড্ডা, সাংস্কৃতিক অনুষ্ঠান, মেয়েদের পিলো পাস, বাচ্চাদের দৌড় প্রতিযোগিতা, র্যাফল ড্রসহ বিভিন্ন আনন্দ-উল্লাসে মেতে ওঠেন গণযোগাযোগ ও সাংবাদিকতা বিভাগের সাবেক শিক্ষার্থীরা।
+            </p>
+        
+            <hr class="my-4" />
+        
+            <p>
+                <span class="text-danger">বিশেষ দ্রঃ </span>: পিকনিকের ছাড়া ১২৫০ টাকা প্রতিজনের 
+            </p>
+            <button type="button" class="btn btn-primary">
+            Participant Now
+            </button>
+        </div>
+        <!-- Hero --> 
+
+
+
+
     </div>
     </div>
     </div>
